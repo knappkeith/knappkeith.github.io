83828ab6af510f3842cde9f6b58c092e0a8235e7
Keith Knapp <knapp.keith@gmail.com>
2015-02-16 20:39:21 -0700

    adding photos and fixing photography page

--- js/pictures.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-
-    $('.cropped-images img').each(function() {
-      if ($(this).width() > $(this).height()) {
-        $(this).addClass('landscape');        
-      }
-    });
-
-});
