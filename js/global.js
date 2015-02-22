46151c1b8a01b01098034e38902a73f9346c73d7
Keith Knapp <knapp.keith@gmail.com>
2015-02-21 21:55:52 -0700

    converting to flexbox, adding photo details

--- js/global.js
+++ js/global.js
@@ -1,9 +1,26 @@
 function getYear() { 
-	d = new Date();
-	n = d.getFullYear();
-	document.getElementById('year').innerHTML = n; 
+    d = new Date();
+    n = d.getFullYear();
+    document.getElementById('year').innerHTML = n; 
 }
 
 jQuery(document).ready(function() {
     jQuery('.nailthumb-container').nailthumb({imageFromWrappingLink: true,});
+});
+
+$(function() {
+    $(".detail-header").click(function () {
+
+        $header = $(this);
+        //getting the next element
+        var $content = $header.next();
+        if ($header.text() == 'Show Details') {
+            $content.show('drop');
+            $header.text('Hide Details');
+        } else {
+            $content.hide('drop');
+            $header.text('Show Details');
+        }
+
+    });
 });
